4fe7e02a9d33b134be019dd572f86cc17c1f8f57
cyh789 <drsggg@naver.com>
2022-05-01 22:17:14 +0900

    브루트포스 1476 날짜계산

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         brute_force bf = new brute_force();
 
 //        brute_force.bf_2309(args);    //아홉난쟁이 문제
-        brute_force.bf_3085(args);  //사탕게임
+//        brute_force.bf_3085(args);  //사탕게임
+        brute_force.bf_1476(args);  //날짜계산
     }
 }
--- src/brute_force/brute_force.java
+++ src/brute_force/brute_force.java
@@ -114,5 +114,56 @@ public class brute_force {
         return ans;
     }
 
-
+    //날짜계산
+    public static void bf_1476(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int e = sc.nextInt() -1;
+        int s = sc.nextInt() -1;
+        int m = sc.nextInt() -1;
+        for (int i = 0; ; i++) {
+            if (i%15==e && i%28==s && i%19==m) {
+                System.out.println(i +1);
+                break;
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
