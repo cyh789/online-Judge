ff9b89d0f9818382b093f520fdfdd4b263222c71
cyh789 <drsggg@naver.com>
2022-05-08 14:38:46 +0900

    14500 테트로미노

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
 //        brute_force.bf_2309(args);    //아홉난쟁이 문제
 //        brute_force.bf_3085(args);  //사탕게임
 //        brute_force.bf_1476(args);  //날짜계산
-            brute_force.bf_1107(args);  //리모컨
+//        brute_force.bf_1107(args);  //리모컨
+        brute_force.bf_14500(args);  //테트로미노
     }
 }
--- src/brute_force/brute_force.java
+++ src/brute_force/brute_force.java
@@ -142,14 +142,24 @@ public class brute_force {
             broken[x] = true; // 트루면 고장!
         }
 
+        //1. N까지 + 또는 - 버튼으로만 움직이는 경우
+        //단순히 Math.abs로 계산하면 된다.
         int ans = Math.abs(n - 100);
 
+        //2. N과 가까운 번호를 누른 후, + 또는 - 버튼으로 움직이는 경우
+        //고장 난 버튼을 제외할 방법을 생각해 주어야 한다.
+        //possible_1107 함수에서 모듈러(%)를 사용하여 한자리씩 고장 난 버튼인지 확인하고,
+        //고장 난 버튼이 하나라도 포함되어 있다면 누를 수 없는 번호이므로 return 0 해준다.
+        //그러고 나서 숫자 버튼 누르는 횟수 + 방향 버튼 누르는 횟수가 더 작은 값인지 확인해 준다.
         for (int i = 0; i <= 999999; i++) {
-            int len = possible(i);
+            //c로 숫자버튼만을 이용해서 이동이 가능한지, 가능하다면 몇 번의 버튼을 누르는지 구하는 변수
+            int len = possible_1107(i);
             if (len > 0) {
+                //숫자버튼으로 n에 최대한 인접하게 이동한후 +또는 -를 몇 번 눌러야 하는지 구하는 변수
                 int press = i - n;
                 if (press < 0) press = -press;
 
+                // 최소 이동 횟수 계산
                 if (ans > len + press) {
                     ans = len + press;
                 }
@@ -158,7 +168,7 @@ public class brute_force {
         System.out.println(ans);
     }
 
-    private static int possible(int c) {
+    private static int possible_1107(int c) {
         if (c == 0) { //0일때 예외처리
             if (broken[0]) {
                 return 0;
@@ -168,13 +178,83 @@ public class brute_force {
         }
         int len = 0;
         while (c > 0) {
+            // 고장난 버튼이 있는 경우
             if (broken[c % 10]) {
                 return 0;
             }
+            // 숫자버튼 누르는 횟수 증가
             len += 1;
             c /= 10;
         }
         return len;
+    }
+
+    //테트로미노
+    static int[][][] BLOCKS_14500 = {
+            //19 4 2
+            {{0,0}, {0,1}, {0,2}, {0,3}}
+            ,{{0,0}, {1,0}, {2,0}, {3,0}}
+            ,{{0,0}, {1,0}, {0,1}, {1,1}}
+            ,{{0,0}, {0,1}, {0,2}, {1,2}}
+            ,{{0,0}, {1,0}, {2,0}, {0,1}}
+            ,{{0,0}, {1,0}, {1,1}, {1,2}}
+            ,{{2,0}, {0,1}, {1,1}, {2,1}}
+            ,{{1,0}, {1,1}, {1,2}, {0,2}}
+            ,{{0,0}, {0,1}, {1,1}, {2,1}}
+            ,{{0,0}, {1,0}, {0,1}, {0,2}}
+            ,{{0,0}, {1,0}, {2,0}, {2,1}}
+            ,{{0,0}, {1,0}, {2,0}, {1,1}}
+            ,{{1,0}, {0,1}, {1,1}, {1,2}}
+            ,{{0,1}, {1,0}, {1,1}, {2,1}}
+            ,{{0,0}, {0,1}, {0,2}, {1,1}}
+            ,{{0,0}, {0,1}, {1,1}, {1,2}}
+            ,{{0,1}, {1,1}, {1,0}, {2,0}}
+            ,{{1,0}, {1,1}, {0,1}, {0,2}}
+            ,{{0,0}, {1,0}, {1,1}, {2,1}}
+    };
+    public static void bf_14500(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] a = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        int ans = 0;
+        //전체 블록을 지정
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                //4개의 큐브로 이루어진 도형 19개를 확인
+                for (int k = 0; k < 19; k++) {
+                    boolean ok = true;
+                    //큐브의 값
+                    int sum = 0;
+                    for (int l = 0; l < 4; l++) {
+                        //큐브의 x좌표
+                        int x = i + BLOCKS_14500[k][l][0];
+                        //큐브의 y좌표
+                        int y = j + BLOCKS_14500[k][l][1];
+
+                        //큐브 4개의 값을 모두 구한다
+                        if (x >= 0 && x < n && y >= 0 && y < m) {
+                            //큐브의 값 = a[x][y]
+                            sum += a[x][y];
+                        } else {
+                            ok = false;
+                            break;
+                        }
+                    }
+
+                    if (ok && ans < sum ) ans = sum;
+                }
+
+            }
+        }
+
+        System.out.println(ans);
     }
 }
 
