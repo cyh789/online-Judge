c35839fae63e899f92e630a24623dea35ecc0fcd
cyh789 <drsggg@naver.com>
2022-05-16 23:45:21 +0900

    2문제 - 브루트포스 건너뛰며해보기

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import brute_force.brute_force;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        brute_force bf = new brute_force();
+    public static void main(String[] args) throws IOException {
+        brute_force brute_force = new brute_force();
 
 //        brute_force.bf_2309(args);    //아홉난쟁이 문제
 //        brute_force.bf_3085(args);  //사탕게임
 //        brute_force.bf_1476(args);  //날짜계산
 //        brute_force.bf_1107(args);  //리모컨
-        brute_force.bf_14500(args);  //테트로미노
+//        brute_force.bf_14500(args);  //테트로미노
+//        brute_force.bf_6064(args);  //카잉달력
+        brute_force.bf_1748(args);  //수 이어 쓰기1
     }
 }
--- src/brute_force/brute_force.java
+++ src/brute_force/brute_force.java
@@ -2,10 +2,16 @@ package brute_force;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class brute_force {
+
+    private static int k;
 
     //아홉난쟁이 문제
     public static void bf_2309(String[] args) {
@@ -14,19 +20,19 @@ public class brute_force {
         int sum = 0;
         int highA = 0;
         int highB = 0;
-        
+
         //난쟁이 9명의 키의 합을 구함
         for (int i = 0; i < intArr.length; i++) {
             intArr[i] = sc.nextInt();
             sum += intArr[i];
         }
-        
+
         //오름차순 정렬
         Arrays.sort(intArr);
 
         //키의 합에서 2명의 키를 뺀 값이 100이라면 break;
-        for (int i = 0; i < intArr.length-1; i++) {
-            for (int j = i+1; j < intArr.length; j++) {
+        for (int i = 0; i < intArr.length - 1; i++) {
+            for (int j = i + 1; j < intArr.length; j++) {
                 if (sum - intArr[i] - intArr[j] == 100) {
                     highA = i;
                     highB = j;
@@ -58,29 +64,29 @@ public class brute_force {
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (j < n-1) {
+                if (j < n - 1) {
                     char t = a[i][j];
-                    a[i][j] = a[i][j+1];
-                    a[i][j+1] = t;
+                    a[i][j] = a[i][j + 1];
+                    a[i][j + 1] = t;
 
                     int temp = getChk_bf_3085(a);
                     if (ans < temp) ans = temp;
 
                     t = a[i][j];
-                    a[i][j] = a[i][j+1];
-                    a[i][j+1] = t;
+                    a[i][j] = a[i][j + 1];
+                    a[i][j + 1] = t;
                 }
-                if (i < n-1) {
+                if (i < n - 1) {
                     char t = a[i][j];
-                    a[i][j] = a[i+1][j];
-                    a[i+1][j] = t;
+                    a[i][j] = a[i + 1][j];
+                    a[i + 1][j] = t;
 
                     int temp = getChk_bf_3085(a);
                     if (ans < temp) ans = temp;
 
                     t = a[i][j];
-                    a[i][j] = a[i+1][j];
-                    a[i+1][j] = t;
+                    a[i][j] = a[i + 1][j];
+                    a[i + 1][j] = t;
                 }
             }
         }
@@ -94,7 +100,7 @@ public class brute_force {
         for (int i = 0; i < n; i++) {
             int cnt = 1;
             for (int j = 1; j < n; j++) {
-                if (a[i][j] == a[i][j-1]) {
+                if (a[i][j] == a[i][j - 1]) {
                     cnt += 1;
                 } else {
                     cnt = 1;
@@ -104,7 +110,7 @@ public class brute_force {
 
             cnt = 1;
             for (int j = 1; j < n; j++) {
-                if (a[j][i] == a[j-1][i]) {
+                if (a[j][i] == a[j - 1][i]) {
                     cnt += 1;
                 } else {
                     cnt = 1;
@@ -119,12 +125,12 @@ public class brute_force {
     //날짜계산
     public static void bf_1476(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int e = sc.nextInt() -1;
-        int s = sc.nextInt() -1;
-        int m = sc.nextInt() -1;
+        int e = sc.nextInt() - 1;
+        int s = sc.nextInt() - 1;
+        int m = sc.nextInt() - 1;
         for (int i = 0; ; i++) {
-            if (i%15==e && i%28==s && i%19==m) {
-                System.out.println(i +1);
+            if (i % 15 == e && i % 28 == s && i % 19 == m) {
+                System.out.println(i + 1);
                 break;
             }
         }
@@ -133,6 +139,7 @@ public class brute_force {
     //리모컨
 
     static boolean[] broken = new boolean[10];
+
     public static void bf_1107(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // 이동 원하는 채널
@@ -192,26 +199,27 @@ public class brute_force {
     //테트로미노
     static int[][][] BLOCKS_14500 = {
             //19 4 2
-            {{0,0}, {0,1}, {0,2}, {0,3}}
-            ,{{0,0}, {1,0}, {2,0}, {3,0}}
-            ,{{0,0}, {1,0}, {0,1}, {1,1}}
-            ,{{0,0}, {0,1}, {0,2}, {1,2}}
-            ,{{0,0}, {1,0}, {2,0}, {0,1}}
-            ,{{0,0}, {1,0}, {1,1}, {1,2}}
-            ,{{2,0}, {0,1}, {1,1}, {2,1}}
-            ,{{1,0}, {1,1}, {1,2}, {0,2}}
-            ,{{0,0}, {0,1}, {1,1}, {2,1}}
-            ,{{0,0}, {1,0}, {0,1}, {0,2}}
-            ,{{0,0}, {1,0}, {2,0}, {2,1}}
-            ,{{0,0}, {1,0}, {2,0}, {1,1}}
-            ,{{1,0}, {0,1}, {1,1}, {1,2}}
-            ,{{0,1}, {1,0}, {1,1}, {2,1}}
-            ,{{0,0}, {0,1}, {0,2}, {1,1}}
-            ,{{0,0}, {0,1}, {1,1}, {1,2}}
-            ,{{0,1}, {1,1}, {1,0}, {2,0}}
-            ,{{1,0}, {1,1}, {0,1}, {0,2}}
-            ,{{0,0}, {1,0}, {1,1}, {2,1}}
+            {{0, 0}, {0, 1}, {0, 2}, {0, 3}}
+            , {{0, 0}, {1, 0}, {2, 0}, {3, 0}}
+            , {{0, 0}, {1, 0}, {0, 1}, {1, 1}}
+            , {{0, 0}, {0, 1}, {0, 2}, {1, 2}}
+            , {{0, 0}, {1, 0}, {2, 0}, {0, 1}}
+            , {{0, 0}, {1, 0}, {1, 1}, {1, 2}}
+            , {{2, 0}, {0, 1}, {1, 1}, {2, 1}}
+            , {{1, 0}, {1, 1}, {1, 2}, {0, 2}}
+            , {{0, 0}, {0, 1}, {1, 1}, {2, 1}}
+            , {{0, 0}, {1, 0}, {0, 1}, {0, 2}}
+            , {{0, 0}, {1, 0}, {2, 0}, {2, 1}}
+            , {{0, 0}, {1, 0}, {2, 0}, {1, 1}}
+            , {{1, 0}, {0, 1}, {1, 1}, {1, 2}}
+            , {{0, 1}, {1, 0}, {1, 1}, {2, 1}}
+            , {{0, 0}, {0, 1}, {0, 2}, {1, 1}}
+            , {{0, 0}, {0, 1}, {1, 1}, {1, 2}}
+            , {{0, 1}, {1, 1}, {1, 0}, {2, 0}}
+            , {{1, 0}, {1, 1}, {0, 1}, {0, 2}}
+            , {{0, 0}, {1, 0}, {1, 1}, {2, 1}}
     };
+
     public static void bf_14500(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -248,7 +256,7 @@ public class brute_force {
                         }
                     }
 
-                    if (ok && ans < sum ) ans = sum;
+                    if (ok && ans < sum) ans = sum;
                 }
 
             }
@@ -256,43 +264,77 @@ public class brute_force {
 
         System.out.println(ans);
     }
+
+    //카잉달력
+    public static void bf_6064(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.valueOf(bf.readLine());
+        while (t-- > 0) {
+            String[] line = bf.readLine().split(" ");
+            int m = Integer.valueOf(line[0]);
+            int n = Integer.valueOf(line[1]);
+            int x = Integer.valueOf(line[2]) - 1;
+            int y = Integer.valueOf(line[3]) - 1;
+            boolean ok = false;
+
+            // 1. n*m => 전체 시행횟수
+            // 그런데 값이 +1 되는게 아니라 m만큼 "건너 뛰며 해보기" 방식이므로 시간복잡도는 O(mn) 이 아니라 O(n)이 된다.
+            // O(mn)의 경우 4만*4만=16억=1.6초 이므로 시간제한에 걸린다.
+
+            // 2. int k = x => k의 초기값은 x
+            // 계산의 편의를 위해 x와 y는 -1씩 해주었으며, 마지막에 결과값에서 +1 해준다.
+
+            // 3. k+=m => m의 배수
+            for (int k = x; k < n * m; k += m) {
+                if (k % n == y) {
+                    System.out.println(k + 1);
+                    ok = true;
+                    break;
+                }
+            }
+            if (!ok) {
+                System.out.println(-1);
+            }
+        }
+    }
+
+    //수 이어 쓰기1
+    public void bf_1748(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(bf.readLine());
+        long ans = 0;
+
+        // 직접 계산하는 방법도 가능하지만, N이 너무 크기 떄문에 실제로 수를 모두 만들면 시간초과
+        // ex) 999까지 그냥 구하면 => 시행횟수가 999이지만, 자릿수로 하면 1자리,2자리,3자리 => 시행횟수가 3
+        // 1억 = 9자리 => 9번만 구하면 됨
+
+        // 1. 자릿수가 1일떄 범위는 1~9이다.
+        // len=1 => 초기 자릿수 1
+        // start=1 => 초기값 1
+
+        // 2. 자릿수가 2일떄 범위는 10~99이다.
+        // len++ => 자릿수 1씩 증가
+        // start*=10 => 초기값 1에서 10배씩 증가
+
+        // 3. end = start*10 -1 => end는 다음 자릿수의 start에서 1을 빼준값과 같다.
+
+        // 4. start<=n => 최대 시행횟수
+        // end = n => end가 n보다 처음 주어진 수보다 클 경우, 처음 주어진 수를 end로 변경한다
+        // ex) 150까지 구하기면 3자릿수의 최대값인 999까지 전부 구할 필요 없이 150까지만 구하면 된다.
+
+        // 5. ans += (long)(end - start + 1) * len;
+        // (end - start + 1) => len 자리 수의 개수
+        // * len => len 자리의 자릿 수
+       for (int start=1, len=1; start<=n; start*=10, len++) {
+            int end = start*10 -1;
+            if (end > n) {
+                end = n;
+            }
+            ans += (long)(end - start + 1) * len;
+        }
+        System.out.println(ans);
+    }
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
