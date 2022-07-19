d6a774eacd430a7df869c7e3d72d4c8202a2cde8
cyh789 <drsggg@naver.com>
2022-07-20 00:11:21 +0900

    220719

---
+++ src/algo_202207/Main_1874.java
@@ -0,0 +1,44 @@
+package algo_202207;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main_1874 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(sc.nextLine());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int num = 1;
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            if (a[i] >= num) {
+                while (a[i] >= num) {
+                    stack.push(num++);
+                    sb.append("+\n");
+                }
+
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                int currNum = stack.pop();
+                if (currNum > a[i]) {
+                    System.out.println("NO");
+                    System.exit(0);
+                } else {
+                    sb.append("-\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+        System.exit(0);
+    }
+}
